de41395cf02e15d63f7d4befee59a6ac313c27e3
Keith Ssewanonda <keith@laboremus.no>
2020-03-17 10:15:59 +0300

    refactor: create helper class for converting JSONfiles data

---
+++ CraftsWebApplication/Core/Helpers/JsonFileConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CraftsWebApplication.Models;
+
+namespace CraftsWebApplication.Core.Helpers
+{
+    public static class JsonFileConverter
+    {
+        public static IEnumerable<Product> GetContent(string fileName)
+        {
+            using (var jsonFileReader = File.OpenText(fileName))
+            {
+                return JsonSerializer.Deserialize<List<Product>>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive  = true
+                    });
+            }
+        }
+
+        public static void WriteContent(string fileName, List<Product> products)
+        {
+            using (var outputStream = File.OpenWrite(fileName))
+            {
+                JsonSerializer.Serialize<List<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+    }
+}
